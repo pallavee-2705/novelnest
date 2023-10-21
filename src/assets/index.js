fd0aa898526f1bb33ef44b0f83cebb2e815e4265
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-10-20 22:25:50 -0400

    added indexing for asset images

---
+++ src/assets/index.js
@@ -0,0 +1,28 @@
+import article1 from "./article1.png"; 
+import article2 from "./article2.png"; 
+import article3 from "./article3.png"; 
+import banner1 from "./banner1.png"; 
+import categories1 from "./categories1.png"; 
+import categories2 from "./categories2.png"; 
+import categories3 from "./categories3.png"; 
+import discount from "./discount.png"; 
+import featurebook from "./featurebook.png"; 
+import newrelease1 from "./newrelease1.png"; 
+import newrelease2 from "./newrelease2.png"; 
+import newrelease3 from "./newrelease3.png"; 
+
+
+export {
+    article1,
+    article2,
+    article3,
+    banner1,
+    categories1,
+    categories2,
+    categories3,
+    discount,
+    featurebook,
+    newrelease1,
+    newrelease2,
+    newrelease3
+}

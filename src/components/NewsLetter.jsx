bc536845ebd5b47ab43c724a8b6b3496b60de679
Shubh <shubhjhawar78@gmail.com>
2023-10-22 15:21:52 -0400

    basic layout of newsletter implemented

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewsLetter = () => {
   return (
-    <div className='relative bg-rose-100 flex flex-col justify-center items-center px-20'>
+    <div className='relative bg-rose-100 flex flex-col justify-center items-center lg:px-20'>
       <div className='absolute left-0 top-0'>Design dot</div>
       <div className='bg-red-500 w-full flex flex-col items-center px-20'>
         <div className="text-white text-4xl max-md:text-2xl font-['Inter'] font-semibold leading-wide tracking-wide pt-14 pb-2 max-md:mb-16">Subscribe to our Newsletter</div>
@@ -11,9 +11,14 @@ const NewsLetter = () => {
         </div>
       </div>
 
-      <div className='bg-white shadow-md w-2/3 mb-10 flex max-md:flex-col items-center p-3 bg-blue-400 -translate-y-10 gap-3'>
-        <div className="w-3/4 max-md:w-full bg-red-300 p-3">Email</div>
-        <div className='3-1/4 max-md:w-full bg-red-300 p-3'>Container</div>
+      <div className='bg-white shadow-md w-2/3 mb-10 flex max-md:flex-col items-center p-3 -translate-y-10 gap-3'>
+        <div className="w-3/4 max-md:w-full p-3 flex items-center gap-3">
+          <div className="max-sm:hidden">✉️</div>
+          <div className='w-full p-2 text-gray-400 rounded-sm border-red-500 transition ease-in-out delay-100 hover:shadow-md hover:border-[1px] hover:-translate-y-1 hover:scale-102  duration-300'>
+            youremail123@gmail.com
+          </div>
+        </div>
+        <div className='w-1/4 max-md:w-full flex flex-col items-center bg-red-500 uppercase p-3 tracking-widest font-semibold text-white'>Subscribe</div>
       </div>
 
       <div className='absolute left-0 bottom-0 bg-cyan-200 w-full'>design dots x 2</div>

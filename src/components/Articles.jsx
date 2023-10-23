49ad8201248520887228e2f2fd8044f1b7dd498a
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-10-23 00:22:40 -0400

    added articles to the component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { article1, article2, article3 } from '../assets';
 
 const Articles = () => {
+  const articles = [
+    {
+      id: 1,
+      title: 'Reading Books Aways Makes The Moments Happy',
+      date: '2nd August 2021',
+      imageUrl: article1
+    },
+    {
+      id: 2,
+      title: 'Great travel at desertReading books always makes the moments happy',
+      date: '2nd August 2021',
+      imageUrl: article2
+    },
+    {
+      id: 3,
+      title: 'Reading books always makes the moments happy',
+      date: '2nd August 2021',
+      imageUrl: article3
+    }
+  ];
+
   return (
     <div className="w-full h-auto bg-slate-50 flex flex-col px-12 py-12 gap-5 items-center" >
       {/* first heading  */}
@@ -12,13 +34,45 @@ const Articles = () => {
         Latest Articles
       </div>
       {/* articles  */}
-      <div>
-        
-      </div>
-      {/* read more button  */}
-      <div>
-        
-      </div>
+
+
+      <div name='main-artcle-div'
+      className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 my-10'>
+        {articles.map(({id, title, date, imageUrl}) => (
+          <div
+          key={id}
+          className='flex-col flex hover:transform hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer'
+          onClick={() => {
+          window.open('https://www.goodreads.com/', '_blank');
+          }}
+          >
+            {/* image  */}
+            <div>
+              <img
+                src={imageUrl}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105 w-full h-auto py-2 px-2 "
+              />
+            </div>
+            {/* date  */}
+            <div className=''>
+              ${date}
+            </div>
+            {/* Title  */}
+            <div>
+              {title}
+            </div>
+            {/* line  */}
+            <div>
+            </div>
+            
+          </div>
+        ))}
+        </div>
+    {/* read more button  */}
+    <div>
+              
+    </div>    
     </div>
   )
 }

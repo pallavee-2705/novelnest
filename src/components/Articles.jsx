fc535c28e4069fa57ed4852e649214a0dc45e4ef
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-10-22 23:30:26 -0400

    initiating articles component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Articles = () => {
   return (
-    <div>Articles</div>
+    <div className="w-full h-auto bg-slate-50 flex flex-col px-12 py-12 gap-5 items-center" >
+      {/* first heading  */}
+      <div className="w-[169px] h-[15px] text-center text-neutral-500 text-[13px] font-medium font-['Inter'] uppercase tracking-wider">
+        Read our articles
+      </div>
+      {/* main heading  */}
+      <div className="mt-2 w-[342px] h-[63px] text-center text-cyan-900 text-5xl font-normal font-['Inter'] capitalize">
+        Latest Articles
+      </div>
+      {/* articles  */}
+      <div>
+        
+      </div>
+      {/* read more button  */}
+      <div>
+        
+      </div>
+    </div>
   )
 }
 

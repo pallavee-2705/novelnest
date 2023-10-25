cf0bc4511490b80e2f7fe154f500c223f6fc14e6
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-10-24 23:57:19 -0400

    made some more changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {footerlogo, ornament2, ornament1} from '../assets';
+import {footerlogo} from '../assets';
 import {FaFacebook, FaYoutube, FaLinkedin} from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 const Footer = () => {

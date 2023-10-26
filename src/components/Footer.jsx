c0843d705c085e17ca7078064a4e98652ef74309
Shubh <shubhjhawar78@gmail.com>
2023-10-25 23:30:01 -0400

    removed unnecessary import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {footerlogo} from '../assets';
 import {FaFacebook, FaYoutube, FaLinkedin} from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
-import { ornament1, ornament2, ornament } from '../assets';
+import { ornament2, ornament } from '../assets';
 const Footer = () => {
   const footerLinks = [
 
